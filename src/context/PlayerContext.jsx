c7536a9bde7ca82a1e96336c0a58287d32ada927
chowankumar <chowanmalhani5@gmail.com>
2024-05-17 16:01:38 +0500

    set dynamic seek bar

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -26,6 +26,7 @@ const PlayerContextProvider = (props) => {
     useEffect(() => {
         setTimeout(() => {
             audioref.current.ontimeupdate = () => {
+        seekBar.current.style.width = (Math.floor(audioref.current.currentTime/audioref.current.duration*100))+"%"
                 setTime({
                     currentTime: {
                         second: Math.floor(audioref.current.currentTime % 60),
